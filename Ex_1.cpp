33d4a2b8ed2e477e0496f82e1796e9f8bc09331f
Hayssa Fonseca <hayssa.fonseca40@gmail.com>
2025-07-07 15:22:09 -0300

    Ex_1.cpp

--- Ex_1.cpp
+++ Ex_1.cpp
@@ -1,41 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Definição da struct Aluno
 typedef struct {
     char nome[50];
-    float notaExercicios;
-    float notaProva1;
-    float notaProva2;
+    float notaExe;
+    float notaP1;
+    float notaP2;
 } Aluno;
 
-// Função para calcular a média final
-float calcularMediaFinal(Aluno a) {
-    float mediaProvas = (a.notaProva1 * 4 + a.notaProva2 * 6) / 10;
-    float mediaFinal = (a.notaExercicios * 0.2) + (mediaProvas * 0.8);
-    return mediaFinal;
+// FunÃ§Ã£o para calcular a mÃ©dia final
+float calculaMediaFinal(Aluno c) {
+    float mProvas = (c.notaP1 * 4 + c.notaP2 * 6) / 10;
+    float mFinal = (c.notaExe * 0.2) + (mProvas * 0.8);
+    return mFinal;
 }
 
-// Função principal para teste
+
 int main() {
     Aluno aluno;
 
-    printf("Digite o nome do aluno (sem espaços): ");
-    scanf("%s", aluno.nome); // Só lê até o primeiro espaço
+    printf("Digite o nome do aluno (sem espaÃ§os): ");
+    scanf("%s", aluno.nome); // SÃ³ lÃª atÃ© o primeiro espaÃ§o
 
     printf("Digite a nota dos exercicios: ");
-    scanf("%f", &aluno.notaExercicios);
+    scanf("%f", &aluno.notaExe);
 
     printf("Digite a nota da Prova 1 (peso 4): ");
-    scanf("%f", &aluno.notaProva1);
+    scanf("%f", &aluno.notaP1);
 
     printf("Digite a nota da Prova 2 (peso 6): ");
-    scanf("%f", &aluno.notaProva2);
+    scanf("%f", &aluno.notaP2);
 
-    float media = calcularMediaFinal(aluno);
+    float me = calculaMediaFinal(aluno);
 
     printf("\nAluno: %s\n", aluno.nome);
-    printf("Media final: %.2f\n", media);
+    printf("Media final: %.2f\n", me);
 
     return 0;
 }
